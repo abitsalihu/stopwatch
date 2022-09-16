e4c1d3c4d648e623b7e10ff77e867d1bef883cd3
Farhan Ali <p200019@pwr.nu.edu.pk>
2022-09-16 05:37:26 +0500

    removed the container and added an id to the toggle div

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const body = document.querySelector("body");
 const timeShow = document.querySelector(".stopwatch__time-show");
 const allBtns = document.querySelector(".stopwatch__btns");
@@ -10,10 +11,10 @@ const resume = document.querySelector(".stopwatch__btns-resume");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
 const milliseconds = document.querySelector(".milliseconds");
-const container = document.querySelector(".container");
 const toggle = document.querySelector(".toggle");
 
 //? STARTING CONDITIONS FOR THE TIMEOUT
+let checkdark = false;
 let timerMilliseconds = 0;
 let timerSeconds = 0;
 let timerMinutes = 0;
@@ -59,22 +60,35 @@ const display = function (name, active, hidden) {
 
 allBtns.addEventListener("click", function (e) {
   if (e.target.classList.contains("stopwatch__btns-start")) {
+    checkdark = false;
     console.log(1);
     body.classList.add("resume-active");
+    if (body.classList.contains("dark")){
+      checkdark = true;
+      body.classList.remove("dark");
+
+    }
     reset.classList.add("reset-new");
     e.target.classList.add("hidden");
     display(stop, "active", "hidden");
     timer();
-  } else if (e.target.classList.contains("stopwatch__btns-reset")) {
+  } 
+  else if (e.target.classList.contains("stopwatch__btns-reset")) {
     console.log(2);
     body.classList.remove("resume-active");
+    
     body.classList.remove("stop-active");
     reset.classList.remove("reset-new");
+    
     display(stop, "hidden", "active");
     display(start, "active", "hidden");
     display(resume, "hidden", "active");
-
+    
     stopTimer();
+    if(checkdark){
+    body.classList.add("dark")
+    checkdark = false;
+    }
     timerMilliseconds = 0;
     timerSeconds = 0;
     timerMinutes = 0;
@@ -98,7 +112,7 @@ allBtns.addEventListener("click", function (e) {
 
 
 toggle.addEventListener("click", () => {
-  container.classList.toggle("dark")
+  body.classList.toggle("dark")
     ? (toggle.firstElementChild.className = "far fa-moon")
     : (toggle.firstElementChild.className = "far fa-sun");
 });
